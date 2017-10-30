f29f324e3ad8435b281733615ca2bafe5211f55c
Nguyễn Nam Long <danhlong88@gmail.com>
2017-10-30 19:03:25 +0700

    Update tag_all_friend_FB.php

--- tag_all_friend_FB.php
+++ tag_all_friend_FB.php
@@ -3,6 +3,7 @@ ini_set('max_execution_time', 0);
 $post_id = "";//id bài muốn bình luận
 $token = "";//token của bạn
 $text = ""; //điền vào đây lời nhắn bạn muốn gửi
+$array_avoid = ["100003219300242","100001518861027","100004053704354","100001914300875","100000217222833"]; //điền id những bạn  mà bạn không muốn tag vào
 $url = "https://graph.facebook.com/me/friends?limit=5000&fields=id&access_token=$token";
 $curl = curl_init();
 curl_setopt_array($curl, array(
@@ -21,13 +22,16 @@ $dem = 0;
 foreach($datas as  $each){
 	$message .= "@[".$each["id"].":0] ";
 	$dem++;
+	if(in_array($each["id"], $array_avoid)) {
+		continue;
+	}
 	//cứ 5 bạn thì sẽ tag 1 lần, tránh bị FB hiểu nhầm spam, và sẽ tự động tag mỗi 10 giây cho đến hết danh sách
 	if($dem == 5){
 		$message .= "
 $text";
+		$curl = curl_init();
 		$message = curl_escape($curl,$message);
 		$url = "https://graph.facebook.com/$post_id/comments?method=post&message=$message&access_token=$token";
-		$curl = curl_init();
 		curl_setopt_array($curl, array(
 			CURLOPT_URL => "$url",
 			CURLOPT_RETURNTRANSFER => false,
@@ -42,3 +46,17 @@ $text";
 		$message = "";
 	}
 }
+$message .= "
+$text";
+$curl = curl_init();
+$message = curl_escape($curl,$message);
+$url = "https://graph.facebook.com/$post_id/comments?method=post&message=$message&access_token=$token";
+curl_setopt_array($curl, array(
+	CURLOPT_URL => "$url",
+	CURLOPT_RETURNTRANSFER => false,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_SSL_VERIFYPEER => false,
+	CURLOPT_SSL_VERIFYHOST => false
+));
+curl_exec($curl);
+curl_close($curl);
