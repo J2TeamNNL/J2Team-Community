6c3f898b89fbcda9329e47b0a22bf8de2d53ed2a
Nguyễn Nam Long <danhlong88@gmail.com>
2017-12-10 12:09:22 +0700

    Update delete_all_post_FB.php

--- delete_all_post_FB.php
+++ delete_all_post_FB.php
@@ -1,8 +1,22 @@
 <?php
 ini_set('max_execution_time', 0);
+//token full quyền
 $token      = "";
+//điền ID nhóm, hoặc trang, hoặc cá nhân
 $id_can_xoa = "";
-$link       = "https://graph.facebook.com/$id_can_xoa/feed?fields=id&limit=5000&access_token=$token";
+//Tùy chỉnh thời gian xóa, điền true nếu muốn chọn tính năng này
+$option = "false";
+if($option=="true"){
+    //Lưu ý điền đúng theo quy tắc năm tháng ngày
+    //Điền thời gian từ ngày bao nhiêu
+    $since = "2016-01-01";
+    //Điền thời gian tới ngày bao nhiêu
+    $until = "2016-12-30";
+    $link  = "https://graph.facebook.com/$id_can_xoa/feed?fields=id&limit=5000&access_token=$token&since=$since&until=$until";
+}
+else{
+   $link = "https://graph.facebook.com/$id_can_xoa/feed?fields=id&limit=5000&access_token=$token"; 
+}
 while (true) {
    $curl    = curl_init();
     curl_setopt_array($curl, array(
