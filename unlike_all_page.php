793d93c45c8d521fc7db1e4bb9ec73b1979f0b5a
Nguyễn Nam Long <danhlong88@gmail.com>
2017-12-22 10:30:30 +0700

    Create unlike_all_page.php

---
+++ unlike_all_page.php
@@ -0,0 +1,49 @@
+<?php 
+ini_set('max_execution_time', 0);
+//token của bạn
+$token = "EAAA...";
+$url = "https://graph.facebook.com/me/likes?limit=100&fields=id&access_token=$token";
+$array_id = array();
+while(true){
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_TIMEOUT => 0,
+	    CURLOPT_SSL_VERIFYPEER => false,
+	    CURLOPT_SSL_VERIFYHOST => false
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
+	$response = json_decode($response,JSON_UNESCAPED_UNICODE);
+	if(isset($response["data"]) && count($response["data"])>0){
+		$array_fb = $response["data"];
+	}
+	else{
+		break;
+	}
+	foreach ($array_fb as $each) {
+		array_push($array_id,$each['id']);
+	}
+	if(!empty($response['paging']['next'])){
+		$url = $response['paging']['next'];
+	}
+	else{
+		break;
+	}
+}
+foreach ($array_id as $each) {
+	$id_page = $each["id"];
+	$url = "https://graph.facebook.com/$id_page/likes?method=delete&access_token=$token";
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => false,
+		CURLOPT_TIMEOUT => 0,
+	    CURLOPT_SSL_VERIFYPEER => false,
+	    CURLOPT_SSL_VERIFYHOST => false
+	));
+	curl_exec($curl);
+	curl_close($curl);
+	sleep(3);
+}
