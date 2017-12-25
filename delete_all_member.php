2d5e664bf761d2ea0abf4a15fe44b0c1fcd2bb44
Nguyễn Nam Long <danhlong88@gmail.com>
2017-12-25 19:27:07 +0700

    Update delete_all_member.php

--- delete_all_member.php
+++ delete_all_member.php
@@ -21,21 +21,20 @@ while (true) {
     $data     = json_decode($response,JSON_UNESCAPED_UNICODE);
     $datas = $data["data"];
     foreach($datas as $each){
-        if(!in_array($array_admin,$each["id"])){
-           $id_mem = $each["id"];
-           $link   = "https://graph.facebook.com/$id_nhom/members?method=delete&member=$id_mem&access_token=$token";
-           $curl   = curl_init();
-           curl_setopt_array($curl, array(
-               CURLOPT_URL => $link,
-               CURLOPT_RETURNTRANSFER => false,
-               CURLOPT_TIMEOUT => 0,
-               CURLOPT_SSL_VERIFYPEER => false,
-               CURLOPT_SSL_VERIFYHOST => false
-           ));
-           curl_exec($curl);
-           curl_close($curl);
-           sleep(5);
-        }
+        if(in_array($array_admin,$each["id"])) continue;
+        $id_mem = $each["id"];
+        $link   = "https://graph.facebook.com/$id_nhom/members?method=delete&member=$id_mem&access_token=$token";
+        $curl   = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $link,
+            CURLOPT_RETURNTRANSFER => false,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false
+        ));
+        curl_exec($curl);
+        curl_close($curl);
+        sleep(5);
     }
     if(!empty($data["paging"]["next"])){
         $link = $data["paging"]["next"];
