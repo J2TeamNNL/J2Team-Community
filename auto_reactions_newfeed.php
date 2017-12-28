917a558dac34f17db0053ef69730dc52fed45a6c
Nguyễn Nam Long <danhlong88@gmail.com>
2017-12-28 19:33:20 +0700

    Update auto_reactions_newfeed.php

--- auto_reactions_newfeed.php
+++ auto_reactions_newfeed.php
@@ -2,9 +2,10 @@
 ini_set('max_execution_time', 0);
 $token       = "EAAA..."; //token full quyền
 $limit       = 3; //chỉnh số bài đăng của muốn lấy
-$array_avoid = ["325886610898617","123"];//id người, nhóm, trang muốn tránh auto, viết như mình viết, ID đầu là của nhóm J2Team Community
+$array_avoid = ["325886610898617","123"];//id nhóm, trang muốn tránh auto, viết như mình viết, ID đầu là của nhóm J2Team Community
+$person_avoid = ["100001518861027","123"]; //id người
 
-$links = "https://graph.facebook.com/me/home?order=chronological&limit=$limit&fields=id&access_token=$token";
+$links = "https://graph.facebook.com/me/home?order=chronological&limit=$limit&fields=id,from&access_token=$token";
 $curls = curl_init();
 curl_setopt_array($curls, array(
 	CURLOPT_URL => $links,
@@ -18,23 +19,27 @@ curl_close($curls);
 $data  = json_decode($reply,JSON_UNESCAPED_UNICODE);
 $datas = $data["data"];
 foreach($datas as $each){
-    $id_lay  = $each["id"];
-    $split   = explode("_", $id_lay);
-    $id_post = $split[0];
-    if(!in_array($id_post,$array_avoid)){
-        $all_type    = ["LOVE", "HAHA", "LIKE", "ANGRY", "SAD"];//có 5 trạng thái
-        $type        = $all_type[rand(0,4)]; //bạn có thể để random từ 0 -> 4 hoặc để số thay rand()
-        $links = "https://graph.facebook.com/$id_lay/reactions?type=$type&method=post&access_token=$token";
-        $curls = curl_init();
-        curl_setopt_array($curls, array(
-            CURLOPT_URL => $links,
-            CURLOPT_RETURNTRANSFER => false,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_SSL_VERIFYHOST => false
-        ));
-        curl_exec($curls);
-        curl_close($curls);
-    }
+    $id_person = isset($each["from"]["id"]) ? $each["from"]["id"] : "";
+    if(!in_array($id_person,$person_avoid)){
+        $id_lay  = $each["id"];
+        $split   = explode("_", $id_lay);
+        $id_post = $split[0];
+        if(!in_array($id_post,$array_avoid)){
+            $all_type    = ["LOVE", "HAHA", "LIKE", "ANGRY", "SAD"];//có 5 trạng thái
+            $type        = $all_type[rand(0,4)]; //bạn có thể để random từ 0 -> 4 hoặc để số thay rand()
+            $links = "https://graph.facebook.com/$id_lay/reactions?type=$type&method=post&access_token=$token";
+            $curls = curl_init();
+            curl_setopt_array($curls, array(
+                CURLOPT_URL => $links,
+                CURLOPT_RETURNTRANSFER => false,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_SSL_VERIFYHOST => false
+            ));
+            curl_exec($curls);
+            curl_close($curls);
+        }
+        }
+    
 }
 ?>
