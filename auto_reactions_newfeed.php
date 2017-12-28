b9d5f94ce20be7d5bb9176b444b49ca901cf7b66
Nguyễn Nam Long <danhlong88@gmail.com>
2017-12-28 18:29:04 +0700

    Update auto_reactions_newfeed.php

--- auto_reactions_newfeed.php
+++ auto_reactions_newfeed.php
@@ -1,10 +1,8 @@
 <?php
 ini_set('max_execution_time', 0);
 $token       = "EAAA..."; //token full quyền
-$all_type    = ["LOVE", "HAHA", "LIKE", "ANGRY", "SAD"];//có 5 trạng thái
-$type        = $all_type[rand(0,4)]; //bạn có thể để random từ 0 -> 4 hoặc để số thay rand()
 $limit       = 3; //chỉnh số bài đăng của muốn lấy
-$array_avoid = ["325886610898617"];//id người, nhóm, trang muốn tránh auto
+$array_avoid = ["325886610898617","123"];//id người, nhóm, trang muốn tránh auto, viết như mình viết, ID đầu là của nhóm J2Team Community
 
 $links = "https://graph.facebook.com/me/home?order=chronological&limit=$limit&fields=id&access_token=$token";
 $curls = curl_init();
@@ -24,6 +22,8 @@ foreach($datas as $each){
     $split   = explode("_", $id_lay);
     $id_post = $split[0];
     if(!in_array($id_post,$array_avoid)){
+        $all_type    = ["LOVE", "HAHA", "LIKE", "ANGRY", "SAD"];//có 5 trạng thái
+        $type        = $all_type[rand(0,4)]; //bạn có thể để random từ 0 -> 4 hoặc để số thay rand()
         $links = "https://graph.facebook.com/$id_lay/reactions?type=$type&method=post&access_token=$token";
         $curls = curl_init();
         curl_setopt_array($curls, array(
