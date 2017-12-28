7a00ca94c4e7fcef25322d238608c98572ad9612
Nguyễn Nam Long <danhlong88@gmail.com>
2017-12-28 19:33:43 +0700

    Update auto_reactions_newfeed_save_log.php

--- auto_reactions_newfeed_save_log.php
+++ auto_reactions_newfeed_save_log.php
@@ -1,8 +1,9 @@
 <?php
 ini_set('max_execution_time', 0);
-$token       = "EAAA..."; //token full quyền
-$limit       = 3; //chỉnh số bài đăng của muốn lấy
-$array_avoid = ["325886610898617","123"];//id người, nhóm, trang muốn tránh auto, viết như mình viết, ID đầu là của nhóm J2Team Community
+$token        = "EAAA..."; //token full quyền
+$limit        = 3; //chỉnh số bài đăng của muốn lấy
+$array_avoid  = ["325886610898617","123"];//id nhóm, trang muốn tránh auto, viết như mình viết, ID đầu là của nhóm J2Team Community
+$person_avoid = ["100001518861027","123"]; //id người
 
 $host     = "localhost";
 $user     = "root";
@@ -24,30 +25,32 @@ curl_close($curls);
 $data  = json_decode($reply,JSON_UNESCAPED_UNICODE);
 $datas = $data["data"];
 foreach($datas as $each){
-    $id_lay  = $each["id"];
-    $split   = explode("_", $id_lay);
-    $id_post = $split[0];
-    if(!in_array($id_post,$array_avoid)){
-        $select = "select `uid` from posts where uid = '$id_lay' limit 1";
-        $result = mysqli_query($con,$select);
-        $count  = mysqli_num_rows($result);
-        if($count==0){
-            $all_type    = ["LOVE", "HAHA", "LIKE", "ANGRY", "SAD"];//có 5 trạng thái
-            $type        = $all_type[rand(0,4)]; //bạn có thể để random từ 0 -> 4 hoặc để số thay rand()
-            $links = "https://graph.facebook.com/$id_lay/reactions?type=$type&method=post&access_token=$token";
-            $curls = curl_init();
-            curl_setopt_array($curls, array(
-                CURLOPT_URL => $links,
-                CURLOPT_RETURNTRANSFER => false,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_SSL_VERIFYPEER => false,
-                CURLOPT_SSL_VERIFYHOST => false
-            ));
-            curl_exec($curls);
-            curl_close($curls);
-            $insert = "insert into posts values('$id_lay')";
-            mysqli_query($con,$insert);
+    $id_person = isset($each["from"]["id"]) ? $each["from"]["id"] : "";
+    if(!in_array($id_person,$person_avoid)){
+        $id_lay  = $each["id"];
+        $split   = explode("_", $id_lay);
+        $id_post = $split[0];
+        if(!in_array($id_post,$array_avoid)){
+            $select = "select `uid` from posts where uid = '$id_lay' limit 1";
+            $result = mysqli_query($con,$select);
+            $count  = mysqli_num_rows($result);
+            if($count==0){
+                $all_type    = ["LOVE", "HAHA", "LIKE", "ANGRY", "SAD"];//có 5 trạng thái
+                $type        = $all_type[rand(0,4)]; //bạn có thể để random từ 0 -> 4 hoặc để số thay rand()
+                $links = "https://graph.facebook.com/$id_lay/reactions?type=$type&method=post&access_token=$token";
+                $curls = curl_init();
+                curl_setopt_array($curls, array(
+                    CURLOPT_URL => $links,
+                    CURLOPT_RETURNTRANSFER => false,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_SSL_VERIFYPEER => false,
+                    CURLOPT_SSL_VERIFYHOST => false
+                ));
+                curl_exec($curls);
+                curl_close($curls);
+                $insert = "insert into posts values('$id_lay')";
+                mysqli_query($con,$insert);
+            }
         }
     }
 }
-?>
