0724ee222fa1ac7dd11fd81948e4b84f2b3bb04b
Nguyễn Nam Long <danhlong88@gmail.com>
2018-02-27 09:44:43 +0700

    Create download_from_nyaa.php

---
+++ download_from_nyaa.php
@@ -0,0 +1,42 @@
+<?php
+set_time_limit(0);
+$links = "https://nyaa.si/?f=0&c=1_0&q=";
+$page  = 0;
+while (true) {
+    $curls = curl_init();
+    curl_setopt_array($curls, array(
+        CURLOPT_URL => $links,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false
+    ));
+    $reply = curl_exec($curls);
+    curl_close($curls);
+    preg_match_all('/\w*\.torrent/', $reply, $matches[]);
+    if(curl_error($curls)){
+        break;
+    }
+    else{
+        $links = "https://nyaa.si/?f=0&c=1_0&q=&p=$page";
+        $page++;
+    }
+}
+foreach ($matches as $multi_array) {
+    foreach ($multi_array as $array) {
+        foreach ($array as $each) {
+            $fp = fopen ("download/$each" , 'w+');
+            $links = "https://nyaa.si/download/$each";
+            $curls = curl_init();
+            curl_setopt_array($curls, array(
+                CURLOPT_URL => $links,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_SSL_VERIFYHOST => false,
+                CURLOPT_FILE => $fp
+            ));
+            curl_exec($curls);
+            curl_close($curls);
+        }
+    }
+}
