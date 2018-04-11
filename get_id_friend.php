3fc47e95ad769c59244e70f0276ca80952780b5f
Nguyễn Nam Long <danhlong88@gmail.com>
2018-04-11 21:00:04 +0700

    Create get_id_friend.php

---
+++ get_id_friend.php
@@ -0,0 +1,36 @@
+<?php 
+ini_set('max_execution_time', 0);
+//token của bạn
+$token = "EAAA...";
+
+$url = "https://graph.facebook.com/me/friends?limit=5000&fields=id&access_token=$token";
+$array_id = array();
+while(true){
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_TIMEOUT => 0,
+	    CURLOPT_SSL_VERIFYPEER => false,
+	    CURLOPT_SSL_VERIFYHOST => false
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
+	$response = json_decode($response,JSON_UNESCAPED_UNICODE);
+	if(isset($response["data"]) && count($response["data"])>0){
+		$array_fb = $response["data"];
+	}
+	else{
+		break;
+	}
+	foreach ($array_fb as $each){
+echo $each['id'].'<br>';
+	}
+	if(!empty($response['paging']['next'])){
+		$url = $response['paging']['next'];
+	}
+	else{
+		break;
+	}
+}
+
