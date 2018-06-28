58ec0b003e38626ce21868c326e34e2f60888502
Nguyễn Nam Long <danhlong88@gmail.com>
2018-06-28 11:10:30 +0700

    Create delete_photo.php

---
+++ delete_photo.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Delete all photos</title>
+</head>
+<body> 
+    <h4>Code xóa toàn bộ ảnh</h4>
+    <h5>Không xóa được ảnh bìa, ảnh đại diện, ảnh được tag</h5>
+    <form method="post">
+        <h3><a href="https://fb.com/me" target="_blank">Cách lấy token: Ấn vào đây => Ctrl U => Ctrl F => Tìm EAAA... (copy đoạn EAAA dài nhất)</a></h3>
+        <input type="text" name="token" placeholder="Nhập token vào đây"><br>
+        Xóa hết<input type="radio" name="all" value="1" checked><br>
+        Xóa theo thời gian<input type="radio" name="all" value="0"><br>
+        Bắt đầu từ ngày nào (có thể để trống nếu muốn xóa hết):<input type="date" name="since""><br>
+        Kết thúc đến ngày nào (có thể để trống nếu muốn xóa hết):<input type="date" name="until""><br>
+        <button name="ok">OK</button>
+    </form>
+</body>
+</html>
+<?php
+if(isset($_POST['ok'])){
+    ini_set('max_execution_time', 0);
+    $token      = $_POST['token'];
+    $option = $_POST['all'];
+    if($option==0){
+        $since = $_POST['since'];
+        $until = $_POST['until'];
+        $link  = "https://graph.facebook.com/me/photos?fields=id&limit=2500&access_token=$token&since=$since&until=$until";
+    }
+    else{
+       $link = "https://graph.facebook.com/me/photos?fields=id&limit=2500&access_token=$token"; 
+    }
+    while (true) {
+       $curl    = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $link,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $data     = json_decode($response,JSON_UNESCAPED_UNICODE);
+        $datas = $data["data"];
+        foreach($datas as $each){
+            $id_lay = $each["id"];
+            $link   = "https://graph.facebook.com/$id_lay?method=delete&access_token=$token";
+            $curl   = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $link,
+                CURLOPT_RETURNTRANSFER => false,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_SSL_VERIFYHOST => false
+            ));
+            curl_exec($curl);
+            curl_close($curl);
+            sleep(rand(2,5));
+            echo "Đã xóa<br>";
+        }
+        if(!empty($data["paging"]["next"])){
+            $link = $data["paging"]["next"];
+        }
+        else{
+            break;
+        }
+    }
+}
+
+?>
