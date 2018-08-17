d2d15e95d372c4c9d2418b8a7879cc82ba18bca0
Nguyễn Nam Long <danhlong88@gmail.com>
2018-08-17 18:52:07 +0700

    Update get_id_fb_by_link.php

--- get_id_fb_by_link.php
+++ get_id_fb_by_link.php
@@ -23,19 +23,22 @@ if(isset($_POST['ok'])){
     ini_set('max_execution_time', 0);
     $token        = $_POST['token'];
     $array_id     = $_POST['array_id'];
-    preg_match_all('/(?<=profile\.php\?id\=)(?<id>[0-9]*)|(?:(?<=\.com\/)|(?<=\.me\/)|(?<=\.co\/)|(?<=\.us\/))(?:(?!profile\.php)(?<username>[\w\.\_]*))/', $array_id, $array);
-    $array_user_name = array_filter($array['username'], function($value) { return $value !== ''; });
-    $array_id        = array_filter($array['id'], function($value) { return $value !== ''; });
-    $total_import    = count($array_user_name);
+    preg_match_all('/(?<=profile\.php\?id\=)([0-9]+)|(?:(?<=\.com)|(?<=\.me)|(?<=\.co)|(?<=\.us))(?:(?:\/groups\/|\/)(?!profile\.php)([\w\.\_]+))/', $array_id, $array, PREG_SET_ORDER);
+    $array_new = array();
+    $array_name = array();
+    foreach ($array as $key => $child_array) {
+    	$array_new[end($child_array)] = 0;
+    }
+    $total_import    = count($array_new);
     $page_limit      = 50;
     $num_page        = ceil($total_import/$page_limit);
-    $array_new_id    = array();
     for($page=0; $page<$num_page; $page++) {
-        $offset  = $page*$page_limit;
-        $fbmaped = array_slice($array_user_name, $offset, $page_limit);
-        $ids     = implode(",", $fbmaped);
-        $link    = "https://graph.facebook.com/?ids=$ids&fields=id&access_token=$token";
-        $curl    = curl_init();
+		$offset  = $page*$page_limit;
+		$fbmaped = array_slice($array_new, $offset, $page_limit);
+		$index   = array_keys($fbmaped);
+		$ids     = implode(",", $index);
+		$link    = "https://graph.facebook.com/?ids=$ids&fields=id,name&access_token=$token";
+		$curl    = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => "$link",
             CURLOPT_RETURNTRANSFER => true,
@@ -46,11 +49,25 @@ if(isset($_POST['ok'])){
         $response = curl_exec($curl);
         curl_close($curl);
         $data     = json_decode($response,JSON_UNESCAPED_UNICODE);
-        foreach ($data as $each) {
-            $array_new_id[] = $each['id'];
-        } 
+        foreach ($data as $key => $each) {
+            $array_new_id[$key] = $each['id'];
+            $array_name[$key]   = $each['name'];
+        }
     }
-    $array_all = array_merge($array_id,$array_new_id);
-    $text      = implode("\n",$array_all);
-    echo "<textarea cols='50' rows='15'>$text</textarea>";
+    echo "<br><br><br><table width='100%' border='1'><tr><td>Tên</td><td>Link đã cấp</td><td>ID Facebook</td>";
+    foreach ($array_new_id as $key => $each) {
+        echo "<tr>";
+        if(isset($array_name[$key])){
+            echo "<td>".$array_name[$key]."</td>";
+            echo "<td><a href='https:fb.com/$key' target='_blank'>$key</td>";
+            echo "<td>".$each."</td>";
+        }
+        else{
+            echo "<td></td>";
+            echo "<td><a href='https:fb.com/$key' target='_blank'>$key</td>";
+            echo "<td></td>";
+        }
+        echo "</tr>";
+    }
+    
 }
