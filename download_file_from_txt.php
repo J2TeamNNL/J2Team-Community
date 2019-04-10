10811c9b50bd7682a523e8e0ce3c503d660ae17f
Nguyễn Nam Long <danhlong88@gmail.com>
2019-04-10 15:18:37 +0700

    Update download_file_from_txt.php

--- download_file_from_txt.php
+++ download_file_from_txt.php
@@ -7,6 +7,8 @@
 	<form method="post" enctype="multipart/form-data">
 		Nhập file txt chứa link ảnh hoặc video (cách nhau xuống dòng) <input type="file" name="file" accept=".txt">
 		<br>
+		Nhập số dòng muốn bỏ cách (có thể để trống) <input type="number" name="number_line">
+		<br>
 		Tên thư mục chứa (có thể để trống) <input type="text" name="folder">
 		<br>
 		<button name="button_submit">OK</button>
@@ -14,10 +16,14 @@
 	<?php 
 		ini_set('max_execution_time', 0);
 		if(isset($_POST['button_submit'])){
-			$file = $_FILES['file'];
-			$folder = empty($_POST['folder']) ? '' : $_POST['folder']."/";
-			$data = file_get_contents($file['tmp_name']);
-			$array = explode(PHP_EOL, $data);
+			$file        = $_FILES['file'];
+			$number_line = empty($_POST['number_line']) ? 0 : $_POST['number_line'];
+			$folder      = empty($_POST['folder']) ? '' : $_POST['folder']."/";
+
+			$data  = file_get_contents($file['tmp_name']);
+			$array = preg_split( '/\r\n|\r|\n/', $data );
+			$array = array_slice($array, $number_line);
+			
 			foreach ($array as $each) {
 				$name_file = substr($each, strrpos($each, '/') + 1);
 				$fp    = fopen ($folder.$name_file , 'w+');
