194de63763cc0d8ef8f410e21a7743b4b5adeb09
Nguyễn Nam Long <danhlong88@gmail.com>
2022-05-21 20:35:04 +0700

    Update scroll_to_bottom.js

--- scroll_to_bottom.js
+++ scroll_to_bottom.js
@@ -1,5 +1,7 @@
-javascript:
-!function(){ 
-var second = prompt("Hãy nhập số giây bạn muốn cuộn"); 
-var scroll_down = setInterval(function(){ window.scrollBy(0,2000); }, 500); 
-setTimeout(function(){ clearInterval(scroll_down);}, second*1000); }();
+javascript:!function(){ 
+  var body = document.body, html = document.documentElement; 
+  var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight ); 
+  var second = prompt("Hãy nhập số giây bạn muốn cuộn"); 
+  var scroll_down = setInterval(function(){ window.scrollBy(0,height); }, 500); 
+  setTimeout(function(){ clearInterval(scroll_down);}, second*1000); 
+}();
